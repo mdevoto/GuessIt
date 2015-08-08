9d3e9d4f5db8801857e24df5c90789b74d0c0939
mdevoto <mdevoto@smu.edu>
2015-08-08 18:35:31 -0500

    Working on facebook friends list and invitable game session next

--- app/src/main/java/com/deevs/guessit/activities/MainActivity.java
+++ app/src/main/java/com/deevs/guessit/activities/MainActivity.java
@@ -1,19 +1,25 @@
 package com.deevs.guessit.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
 
 import com.deevs.guessit.R;
-import com.facebook.AccessToken;
-import com.facebook.FacebookSdk;
+import com.deevs.guessit.views.TypefaceTextView;
 import com.facebook.appevents.AppEventsLogger;
 
 public class MainActivity extends Activity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        setupClickListeners();
     }
 
     @Override
@@ -30,5 +36,22 @@ public class MainActivity extends Activity {
 
         // Logs 'app deactivate' App Event.
         AppEventsLogger.deactivateApp(this);
+    }
+
+    private void setupClickListeners() {
+        final TypefaceTextView createGameBtn = (TypefaceTextView) findViewById(R.id.create_game);
+        createGameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Runnable gameLobbyRunnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        final Intent startLobbyIntent = new Intent(getApplicationContext(), GameLobby.class);
+                        startActivity(startLobbyIntent);
+                    }
+                };
+                new Handler().post(gameLobbyRunnable);
+            }
+        });
     }
 }
